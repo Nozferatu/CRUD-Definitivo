2a807588fdc1c050c6145c02861a4ed45a9f3f70
Nozferatu <sparrowhawk2005@gmail.com>
2025-01-13 16:07:22 +0100

    Clases de entidad

---
+++ app/src/main/java/com/cmj/crud_definitivo/entity/Guitarra.kt
@@ -0,0 +1,16 @@
+package com.cmj.crud_definitivo.entity
+
+import java.io.Serializable
+
+data class Guitarra(
+    var key: String? = "",
+    var fechaCreacion: String? = "2000-01-01",
+    var idImagen: String? = "",
+    var urlImagen: String? = "",
+    var nombre: String? = "",
+    var descripcion: String? = "",
+    var marca: String? = "",
+    var modelo: String? = "",
+    var rating: Float? = 5f,
+    var precio: Float? = 100f
+): Serializable
