ae673ac01d85ee4d75222183918adddd63a2f68e
Nozferatu <sparrowhawk2005@gmail.com>
2025-01-14 18:54:34 +0100

    Ya se muestran las guitarras que existen

--- app/src/main/java/com/cmj/crud_definitivo/vistas/ListadoGuitarrasActivity.kt
+++ app/src/main/java/com/cmj/crud_definitivo/vistas/ListadoGuitarrasActivity.kt
@@ -4,24 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cmj.crud_definitivo.crud.GuitarraCRUD
+import com.cmj.crud_definitivo.entity.Guitarra
 import com.cmj.crud_definitivo.ui.theme.CRUDDefinitivoTheme
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.database
 
 class ListadoGuitarrasActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val listaGuitarras = mutableStateListOf<Guitarra>()
+
+        FirebaseApp.initializeApp(applicationContext)
+        val dbRef = Firebase.database.reference
+        val guitarraCRUD = GuitarraCRUD(applicationContext, dbRef)
+
+        guitarraCRUD.recuperarGuitarras { guitarras ->
+            listaGuitarras.addAll(guitarras)
+        }
+
         setContent {
             CRUDDefinitivoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ListadoGuitarras(
+                        modifier = Modifier.padding(innerPadding),
+                        listaGuitarras
                     )
                 }
             }
@@ -30,9 +54,26 @@ class ListadoGuitarrasActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ListadoGuitarras(modifier: Modifier = Modifier, guitarras: SnapshotStateList<Guitarra>) {
+    LazyColumn(modifier = modifier) {
+        items(guitarras) { guitarra ->
+            Guitarra(guitarra)
+        }
+    }
+}
+
+@Composable
+fun Guitarra(guitarra: Guitarra) {
+    Card(
+        modifier = Modifier
+            .padding(vertical = 10.dp, horizontal = 20.dp)
+            .fillMaxWidth()
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+        ) {
+            Text(guitarra.nombre)
+        }
+    }
 }
