e03bf909972ec8963454f4737031833341d730c1
Nozferatu <sparrowhawk2005@gmail.com>
2025-01-16 20:58:03 +0100

    Ya se guardan las guitarras

--- app/src/main/java/com/cmj/crud_definitivo/vistas/PersistirGuitarraActivity.kt
+++ app/src/main/java/com/cmj/crud_definitivo/vistas/PersistirGuitarraActivity.kt
@@ -2,6 +2,7 @@ package com.cmj.crud_definitivo.vistas
 
 import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -26,8 +27,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -47,7 +50,11 @@ import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.database
 import io.appwrite.Client
+import io.appwrite.ID
+import io.appwrite.models.InputFile
 import io.appwrite.services.Storage
+import kotlinx.coroutines.launch
+import java.util.Calendar
 
 class PersistirGuitarraActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,17 +79,20 @@ class PersistirGuitarraActivity : ComponentActivity() {
 @Composable
 fun PersistirGuitarra(guitarra: Guitarra, modifier: Modifier = Modifier) {
     val contexto = LocalContext.current
+    val contentResolver = contexto.contentResolver
+    val coroutineScope = rememberCoroutineScope()
+
     val colorBoton = ButtonDefaults.buttonColors(
         containerColor = Purple80
     )
 
     //Tema DB
     val databaseRef = Firebase.database.reference
-    var guitarraCRUD = GuitarraCRUD(contexto, databaseRef)
+    val guitarraCRUD = GuitarraCRUD(contexto, databaseRef)
 
     //Tema AppWrite
     val id_proyecto = "6762fbc00010d599c17c"
-    var id_bucket = "6762fbed003a60f5f03f"
+    val id_bucket = "6762fbed003a60f5f03f"
     val client = Client()
         .setEndpoint("https://cloud.appwrite.io/v1")
         .setProject(id_proyecto)
@@ -107,6 +117,7 @@ fun PersistirGuitarra(guitarra: Guitarra, modifier: Modifier = Modifier) {
     var marca: String by rememberSaveable { mutableStateOf("") }
     var modelo: String by rememberSaveable { mutableStateOf("") }
     var precio: String by rememberSaveable { mutableStateOf("") }
+    val rating = remember { mutableFloatStateOf(guitarra.rating) }
 
     Column(modifier = modifier
         .fillMaxWidth()
@@ -153,6 +164,14 @@ fun PersistirGuitarra(guitarra: Guitarra, modifier: Modifier = Modifier) {
             singleLine = true
         )
 
+        StarRatingBar(modifier = Modifier,
+            maxStars = 5,
+            rating = rating.floatValue,
+            onRatingChanged = {
+                rating.floatValue = it
+            }
+        )
+
         Button(modifier = Modifier.padding(vertical = 20.dp),
             onClick = {
                 launcher.launch("image/*")
@@ -183,7 +202,57 @@ fun PersistirGuitarra(guitarra: Guitarra, modifier: Modifier = Modifier) {
                 colors = colorBoton,
 
                 onClick = {
+                    val calendar = Calendar.getInstance()
+                    val fechaActual = StringBuilder()
+                        .append(calendar.get(Calendar.YEAR)).append("-")
+                        .append(calendar.get(Calendar.MONTH) + 1).append("-")
+                        .append(calendar.get(Calendar.DATE)).toString()
 
+                    coroutineScope.launch{
+                        var nombreArchivo = ""
+                        val inputStream = contentResolver.openInputStream(url_imagen!!)
+                        val aux = contentResolver.query(url_imagen!!, null, null, null, null)
+                        aux.use {
+                            if (it!!.moveToFirst()) {
+                                // Obtener el nombre del archivo
+                                val nombreIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                                if (nombreIndex != -1) {
+                                    nombreArchivo = it.getString(nombreIndex)
+                                }
+                            }
+                        }
+                        val mimeType: String = contentResolver.getType(url_imagen!!).toString()
+
+                        val fileInput = InputFile.fromBytes(
+                            bytes = inputStream?.readBytes() ?: byteArrayOf(),
+                            filename = nombreArchivo,
+                            mimeType = mimeType
+                        )
+
+                        val idFile = ID.unique()
+                        bucket.createFile(
+                            bucketId = id_bucket,
+                            fileId = idFile,
+                            file = fileInput
+                        )
+
+                        val urlImagen =
+                            "https://cloud.appwrite.io/v1/storage/buckets/$id_bucket/files/$idFile/preview?project=$id_proyecto"
+
+                        val guitarraAPersistir = Guitarra("",
+                            fechaActual,
+                            idFile,
+                            urlImagen,
+                            nombre,
+                            descripcion,
+                            marca,
+                            modelo,
+                            rating.floatValue,
+                            precio.toFloatOrNull() ?: 0f
+                        )
+
+                        guitarraCRUD.persistirGuitarra(guitarraAPersistir)
+                    }
                 }
             ) { Text("Agregar guitarra", color = Purple40) }
         }
