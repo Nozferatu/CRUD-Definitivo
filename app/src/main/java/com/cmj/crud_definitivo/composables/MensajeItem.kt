4e0d6e511a548cf352f10b5cadc24de604b15a6c
Nozferatu <sparrowhawk2005@gmail.com>
2025-01-21 23:57:14 +0100

    Imports basura

--- app/src/main/java/com/cmj/crud_definitivo/composables/MensajeItem.kt
+++ app/src/main/java/com/cmj/crud_definitivo/composables/MensajeItem.kt
@@ -1,17 +1,11 @@
 package com.cmj.crud_definitivo.composables
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
